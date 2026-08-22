133e084a7aea8e65421c89280ce66555efdedc53
Vinicius Verneck <vini_verneck@hotmail.com>
2026-08-21 21:47:43 -0300

    aula de python 21082026
    aula de python 21082026

---
+++ Aula de Python/app.py
@@ -0,0 +1,77 @@
+'''nome = "Vinicius"
+idade = 2026-1981
+altura = 1.74
+cidade = "São Paulo"
+anos = 100-idade
+
+print("Nome: " + nome + " | Idade = " + str(idade) + " | Altura: " + str(altura) + " | Mora em: " + cidade)
+
+print("Faltam " + str(anos) + " anos para completar 100 anos de idade.")
+
+print(altura >= 1.70)'''
+
+'''preco_produto = 45.90
+quantidade = 3
+desconto = 0.10
+
+valorTotal = preco_produto * quantidade
+
+print("Valor total sem desconto: R$" + str(valorTotal))
+
+valordesconto = valorTotal * desconto
+
+print("Desconto: R$" + str(valordesconto))
+
+totalcomdesconto = valorTotal - valordesconto
+
+print("Valor total com desconto de 10% : R$" + str(totalcomdesconto))
+
+troco = 150 - totalcomdesconto
+
+print("Troco: R$" + str(troco))
+
+pagamento  = float(input("Insira um valor: "))
+
+troco = pagamento - totalcomdesconto
+
+print("Troco: R$" + str(troco))
+
+nota_50 = troco //50
+sobra = troco % 50
+
+print("Notas: " + str(nota_50) + " Sobra: " + str(sobra))'''
+
+
+notas = [7.5, 8.0, 6.5, 9.0, 5.5]
+'''Mostrar notas 0 e a ultima'''
+print(notas[0], notas[4])
+
+'''Adicionar notas'''
+notas.append(11.5)
+
+print(notas)
+'''Contas quantas notas notas'''
+print(len(notas))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Aula de Python/aula04.py
@@ -0,0 +1,8 @@
+# use o operador % para
+# verificar se o resto da
+# divisão por 3 é igual a 0
+# n % 3 -- 0
+
+for n in range(1, 21):
+    if n % 3 == 0:
+        print(n)
